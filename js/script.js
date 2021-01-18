934759ae6dcbe9ce0010c8f18eb10396420ce2fb
Viktor Komarov <vitya8989@mail.ru>
2021-01-18 22:43:03 +0300

    mneojkwer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,118 @@
-function testWebP(e){var t=new Image;t.onload=t.onerror=function(){e(2==t.height)},t.src="data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"}testWebP((function(e){1==e?document.querySelector("body").classList.add("webp"):document.querySelector("body").classList.add("no-webp")}));const dmtMove=document.querySelector(".move__img"),dmtMoveFront=document.querySelector(".move__img-front"),mediaQuery=window.matchMedia("(min-width: 769px)");if(mediaQuery.matches){function MoveBackground(e){let t=e.clientX/window.innerWidth*-30+15,n=e.clientY/window.innerHeight*-20+10,o=e.clientX/window.innerWidth*-60+30,a=e.clientY/window.innerHeight*-40+20;dmtMove.setAttribute("style","transform: translate("+t+"px, "+n+"px);"),dmtMoveFront.setAttribute("style","transform: translate("+o+"px, "+a+"px);")}document.addEventListener("mousemove",(function(e){MoveBackground(e)}))}let playArea=document.querySelectorAll(".play-area"),playAreaMobile=document.querySelector(".main__img"),track=document.querySelector(".track"),playIcon=document.querySelector(".play-icon"),links=document.querySelectorAll(".content__link");if(playArea.length>0)for(let e=0;e<playArea.length;e++)playArea[e].onclick=function(){track.classList.contains("play")?pauseTrack():playTrack()};for(let e=0;e<links.length;e++)links[e].onclick=function(){track.classList.contains("play")&&pauseTrack()};function pauseTrack(){track.pause(),track.classList.remove("play"),playIcon.classList.remove("active")}function playTrack(){track.play(),track.classList.add("play"),playIcon.classList.add("active")}const mediaQueryMobile=window.matchMedia("(max-width: 768px)");if(mediaQueryMobile.matches){document.querySelector(".images__mobile").appendChild(playIcon);let e=document.querySelector(".content__links");document.querySelector(".content").appendChild(e);let t=document.querySelector(".content__top"),n=document.querySelector(".content__subtitle");t.appendChild(n);let o=document.querySelector(".move__img-mobile"),a=document.querySelector(".move__img-front-mobile");function handleOrientation(e){let t=e.beta,n=e.gamma;t>90&&(t=90),t<0&&(t=0),t+=90,n+=90;let c=-120*t/180+60,i=-60*n/180+30,r=-180*t/180+120,l=-90*n/180+45;o.setAttribute("style","transform: translate("+i+"px, "+c+"px);"),a.setAttribute("style","transform: translate("+l+"px, "+r+"px);")}window.addEventListener("deviceorientation",handleOrientation)}
+function testWebP(callback) {
+
+var webP = new Image();
+webP.onload = webP.onerror = function () {
+callback(webP.height == 2);
+};
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+if (support == true) {
+document.querySelector('body').classList.add('webp');
+}else{
+document.querySelector('body').classList.add('no-webp');
+}
+});;
+
+const dmtMove = document.querySelector('.move__img');
+const dmtMoveFront = document.querySelector('.move__img-front');
+const mediaQuery = window.matchMedia('(min-width: 769px)');
+if (mediaQuery.matches) {
+   document.addEventListener("mousemove", function (e) { MoveBackground(e); });
+
+   function MoveBackground(e) {
+      let offsetX = (e.clientX / window.innerWidth * -30) + 15;
+      let offsetY = (e.clientY / window.innerHeight * -20) + 10;
+      let offsetXfast = (e.clientX / window.innerWidth * -60) + 30;
+      let offsetYfast = (e.clientY / window.innerHeight * -40) + 20;
+      dmtMove.setAttribute("style", "transform: translate(" + offsetX + "px, " + offsetY + "px);");
+      dmtMoveFront.setAttribute("style", "transform: translate(" + offsetXfast + "px, " + offsetYfast + "px);");
+   }
+}
+
+let playArea = document.querySelectorAll('.play-area');
+let playAreaMobile = document.querySelector('.main__img');
+let track = document.querySelector('.track');
+let playIcon = document.querySelector('.play-icon');
+let links = document.querySelectorAll('.content__link');
+
+if (playArea.length > 0) {
+   for (let i = 0; i < playArea.length; i++) {
+      playArea[i].onclick = function () {
+         if (track.classList.contains('play')) {
+            pauseTrack();
+         } else {
+            playTrack();
+         }
+      };
+   }
+}
+for (let i = 0; i < links.length; i++) {
+   links[i].onclick = function () {
+      if (track.classList.contains('play')) {
+         pauseTrack();
+      }
+   }
+}
+function pauseTrack() {
+   track.pause();
+   track.classList.remove('play');
+   playIcon.classList.remove('active');
+}
+function playTrack() {
+   track.play();
+   track.classList.add('play');
+   playIcon.classList.add('active');
+}
+
+const mediaQueryMobile = window.matchMedia('(max-width: 768px)');
+if (mediaQueryMobile.matches) {
+   let mobileImages = document.querySelector('.images__mobile');
+   mobileImages.appendChild(playIcon);
+   let linksBox = document.querySelector('.content__links');
+   let content = document.querySelector('.content');
+   content.appendChild(linksBox);
+   let contentTop = document.querySelector('.content__top');
+   let contentSubtitle = document.querySelector('.content__subtitle');
+   contentTop.appendChild(contentSubtitle);
+
+
+   let backMobile = document.querySelector('.move__img-mobile');
+   let frontMobile = document.querySelector('.move__img-front-mobile');
+
+   function handleOrientation(event) {
+      let x = event.beta;
+      let y = event.alpha;
+
+      if (x > 120) { x = 120 };
+      if (x < -60) { x = -60 };
+      if (y > 90) { y = 90 };
+      if (y < -90) { y = -90 };
+
+      x += 60;
+      y += 90;
+
+      let osX = (-60 * x / 180 + 30);
+      let osY = (-60 * y / 180 + 30);
+      let osXFast = (-120 * x / 180 + 60);
+      let osYFast = (-120 * y / 180 + 60);
+
+      let isiDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
+
+      if (isiDevice) {
+         backMobile.style = "-webkit-transform: translate(" + osY + "px, " + osX + "px);";
+         frontMobile.style = "-webkit-transform: translate(" + osYFast + "px, " + osXFast + "px);";
+      } else {
+         backMobile.style = "transform: translate(" + osY + "px, " + osX + "px);";
+         frontMobile.style = "transform: translate(" + osYFast + "px, " + osXFast + "px);";
+      }
+   }
+
+   window.addEventListener('deviceorientation', handleOrientation);
+   let isi = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
+   if (isi) {
+      alert('это iphone');
+   }
+}
